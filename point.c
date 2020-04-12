0792d62ccc755c349c7398e7f061cb3e617decb7
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-04-12 19:02:11 +0530

    made typedef for struct in order to avoid repeatation

--- point.c
+++ point.c
@@ -1,14 +1,14 @@
 #include <math.h>
 #include "point.h"
 
-unsigned int get_distance(struct Point first_point, struct Point second_point)
+unsigned int get_distance(Point first_point, Point second_point)
 {
   int x = pow(first_point.x - second_point.x, 2);
   int y = pow(first_point.y - second_point.y, 2);
   return sqrt(x + y);
 }
 
-void get_closest_food(struct Point food_points[], int points_length, struct Point current_location, struct Point *closest_food_location)
+void get_closest_food(Point food_points[], int points_length, Point current_location, Point *closest_food_location)
 {
   double distance = get_distance(current_location, food_points[0]);
   closest_food_location->x = food_points[0].x;
