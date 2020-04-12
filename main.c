ba5480b80313809dd5a874a6ececa837540630f8
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-04-12 19:09:59 +0530

    modifed the main added food-points

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
+#include <stdio.h>
 #include "point.h"
 
 int main(void)
 {
+  Point food_points[5] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}};
+  int points_length = 5;
+  Point current_location[] = {{18, 86}, {97, 27}, {69, 7}, {10, 94}, {36, 27}};
+  Point closest_food_location = {0, 0};
+  for (int i = 0; i < 5; i++)
+  {
+    get_closest_food(food_points, points_length, current_location[i], &closest_food_location);
+    printf("Location of organism: [%d %d],  Closest food target : [%d %d]\n", current_location[i].x, current_location[i].y, closest_food_location.x, closest_food_location.y);
+  }
   return 0;
 }
--- point.c
+++ point.c
@@ -10,7 +10,18 @@ unsigned int get_distance(Point first_point, Point second_point)
 
 void get_closest_food(Point food_points[], int points_length, Point current_location, Point *closest_food_location)
 {
-  double distance = get_distance(current_location, food_points[0]);
+  unsigned int distance = get_distance(current_location, food_points[0]);
   closest_food_location->x = food_points[0].x;
   closest_food_location->y = food_points[0].y;
+
+  for (int i = 1; i < points_length; i++)
+  {
+    unsigned int current_distance = get_distance(current_location, food_points[i]);
+    if (current_distance < distance)
+    {
+      distance = current_distance;
+      closest_food_location->x = food_points[i].x;
+      closest_food_location->y = food_points[i].y;
+    }
+  }
 }
